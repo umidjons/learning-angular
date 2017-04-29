c9f22ec9e316c21c0d9903fa90dacc3086414b6d
Umidjons <almatov.us@gmail.com>
2017-04-29 16:03:24 +0500

    Articles rendered via app-article component.

--- angular-reddit/src/app/article/article.component.ts
+++ angular-reddit/src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
@@ -10,10 +10,9 @@ export class ArticleComponent implements OnInit {
 
     @HostBinding('attr.class') cssClass = 'row';
 
-    article: Article;
+    @Input() article: Article;
 
     constructor() {
-        this.article = new Article('Angular 2', 'http://angular.io', 10);
     }
 
     voteUp(): boolean {
